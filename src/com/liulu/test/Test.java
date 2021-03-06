76b1abfb564e8129a86bb4231018489a5e9af196
““liuluswag <394267655@qq.com>
2021-03-06 13:00:54 +0800

    新增寻找下一个更大元素

--- src/com/liulu/test/Test.java
+++ src/com/liulu/test/Test.java
@@ -1,15 +1,47 @@
 package com.liulu.test;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 public class Test {
 
-    public static void test(){
-        Object object1 = new Object();
-        Object object2 = new Object();
-        System.out.println(object1.hashCode() == object2.hashCode());
+
+    public static void reverseString(char[] s) {
+        char temp = ' ';
+        int n = s.length;
+        for(int i = 0; i < n / 2; i++){
+            temp = s[i];
+            s[i] = s[n - i - 1];
+            s[n - i - 1] = temp;
+        }
+    }
+
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[nums1.length];
+        for(int i = 0; i < nums2.length; i++){
+            while(!stack.empty() && nums2[i] > stack.peek()){
+                map.put(stack.pop(), nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+        while(!stack.empty()){
+            map.put(stack.pop(), -1);
+        }
+        for(int i = 0; i < nums1.length; i++){
+            res[i] = map.get(nums1[i]);
+        }
+        return res;
     }
 
     public static void main(String[] args) {
-        test();
+        int[] nums1 = new int[]{2,4};
+        int[] nums2 = new int[]{1,2,3,4};
+        int[] res = nextGreaterElement(nums1, nums2);
+        for (int i : res){
+            System.out.println(i);
+        }
     }
 
 }
